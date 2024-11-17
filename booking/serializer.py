011cd9b4ce9c6fa8bef7d26e7dacb0245fda99e0
Vlad <vladvoytkiv321@gmail.com>
2024-11-17 18:08:33 +0200

    /

--- booking/serializer.py
+++ booking/serializer.py
@@ -2,12 +2,46 @@ from rest_framework import serializers
 
 from .models import Hotel, Flight, Car, Booking
 
+
+class HotelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hotel
+        fields = ['name', 'location', 'date_of_arrival', 'departure_date', 'room_type']
+
+
+class CarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Car
+        fields = ['make', 'model', 'year', 'rental_start_date', 'rental_end_date']
+
+
+class FlightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Flight
+        fields = ['airline', 'flight_number', 'date_of_departure', 'departure_time']
+
+
 class BookingSerializer(serializers.ModelSerializer):
+    hotel_details = HotelSerializer(source='hotel', read_only=True)
+    car_details = CarSerializer(source='car', read_only=True)
+    flight_details = FlightSerializer(source='flight', read_only=True)
+
     class Meta:
         model = Booking
-        fields = ['id', 'created_at', 'user', 'status', 'other_related_field']
-        read_only_fields = ['id', 'created_at', 'user']
+        fields = ['created_at', 'created_by', 'status', 'hotel', 'car', 'flight', 'hotel_details', 'car_details',
+                  'flight_details']
 
-    def create(self, validated_data):
-        validated_data['user'] = self.context['request'].user
-        return super().create(validated_data)
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+
+        if not instance.hotel:
+            representation.pop('hotel_details', None)
+        if not instance.car:
+            representation.pop('car_details', None)
+        if not instance.flight:
+            representation.pop('flight_details', None)
+
+        if instance.status == 'CANCELLED':
+            representation['status_message'] = 'This booking has been cancelled.'
+
+        return representation
--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from rest_framework import generics
+from requests import Response
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
 
 from booking.models import Booking
 from booking.serializer import BookingSerializer
@@ -8,5 +9,18 @@ from booking.serializer import BookingSerializer
 class CreateBookingView(generics.CreateAPIView):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
+
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            {"message": "Booking created successfully", "data": serializer.data},
+            status=status.HTTP_201_CREATED,
+            headers=headers
+        )
